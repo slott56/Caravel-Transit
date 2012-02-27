de248b6a27b4470e94fb04a26e3f4f32a0cef649
S.Lott <slott56@gmail.com>
2012-02-27 13:44:41 -0500

    Rename master document to cooperate with github pages.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'caravel'
 
 # General information about the project.
 project = u'Caravel-Transit'
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Caravel.tex', u'Caravel-Transit Documentation',
+  ('caravel', 'Caravel.tex', u'Caravel-Transit Documentation',
    u'S.Lott', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'caravel', u'Caravel-Transit Documentation',
+    ('caravel', 'caravel', u'Caravel-Transit Documentation',
      [u'S.Lott'], 1)
 ]
 
@@ -227,7 +227,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Caravel', u'Caravel-Transit Documentation',
+  ('caravel', 'Caravel', u'Caravel-Transit Documentation',
    u'S.Lott', 'Caravel', 'Read and Analyze Transit GPS Data.',
    'Miscellaneous'),
 ]
